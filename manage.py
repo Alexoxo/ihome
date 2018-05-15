0ec4ce11acf1359dc6b1e65faa3977a6fb135674
Alexoxo <ggiandfang@gmail.com>
2018-05-15 20:11:14 +0800

    项目初始化&git管理

--- manage.py
+++ manage.py
@@ -1,8 +1,17 @@
 # coding=utf-8
 from flask import Flask
 
+
+class Config(object):
+    """配置类"""
+    DEBUG = True
+
+
+
 # 创建Flask应用程序实例
 biu = Flask(__name__)
+
+biu.config.from_object(Config)
 
 
 @biu.route('/')
@@ -12,4 +21,4 @@ def index():
 
 if __name__ == '__main__':
     # 运行开发web服务器
-    biu.run(debug=True)
+    biu.run()
