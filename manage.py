13e917aca202647b8536ded9d49fc66bcc6799c4
Alexoxo <ggiandfang@gmail.com>
2018-05-15 20:31:50 +0800

    集成mysql数据库

--- manage.py
+++ manage.py
@@ -1,17 +1,26 @@
 # coding=utf-8
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 
 class Config(object):
     """配置类"""
     DEBUG = True
+    # mysql相关配置
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:password@127.0.0.1:3306/ihome_db'  # 设置数据库链接地址
+    SQLALCHEMY_TRACK_MODIFICATIONS = False  # 关闭追踪数据库的修改
 
 
 
 # 创建Flask应用程序实例
 biu = Flask(__name__)
 
+# 由类中导入配置
 biu.config.from_object(Config)
+
+# 创建sqlAlchemy对象
+d_base = SQLAlchemy(biu)
+
 
 
 @biu.route('/')
